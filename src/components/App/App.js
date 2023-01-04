df5c17c742eec7bb9f38cafcfcee530e782440f0
Nathan <nathanbailie.dev@gmail.com>
2023-01-04 13:06:50 +0300

    addded additional images to the main componentm changed some styles and add enter button event to the button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -32,25 +32,26 @@ const App = () => {
 	function onGetWeather(cityName) {
 		setMainLoading(true);
 		setForecastLoading(true);
+		console.log(123)
 
-		setTimeout(() => {
-			freeWeatherApiSerice
-				.getCurrentWeather(cityName)
-				.then(response => {
-					setWeatherMain(response.main);
-					setWeatherConditions(response.conditions);
-					setMainLoading(false);
-				})
-				.catch(() => { setError(true); setMainLoading(false) })
+		// setTimeout(() => {
+		// 	freeWeatherApiSerice
+		// 		.getCurrentWeather(cityName)
+		// 		.then(response => {
+		// 			setWeatherMain(response.main);
+		// 			setWeatherConditions(response.conditions);
+		// 			setMainLoading(false);
+		// 		})
+		// 		.catch(() => { setError(true); setMainLoading(false) })
 
-			openWeatherMapService
-				.getWeatherForecast(cityName)
-				.then(response => {
-					setweatherForecast(response);
-					setForecastLoading(false);
-				})
-				.catch(() => { setForecastError(true); setForecastLoading(false) })
-		}, 1000);
+		// 	openWeatherMapService
+		// 		.getWeatherForecast(cityName)
+		// 		.then(response => {
+		// 			setweatherForecast(response);
+		// 			setForecastLoading(false);
+		// 		})
+		// 		.catch(() => { setForecastError(true); setForecastLoading(false) })
+		// }, 1000);
 	}
 
 	if (error) {
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,24 +1,58 @@
 import './main.scss';
-import main from '../../resources/img/main.jpg';
-import { useState } from 'react';
+import main1 from '../../resources/img/main/main1.jpg';
+import main2 from '../../resources/img/main/main2.jpg';
+import main3 from '../../resources/img/main/main3.jpg';
+import main4 from '../../resources/img/main/main4.jpg';
+import main5 from '../../resources/img/main/main5.jpg';
+import main6 from '../../resources/img/main/main6.jpg';
+import main7 from '../../resources/img/main/main7.jpg';
+import main8 from '../../resources/img/main/main8.jpg';
+import main9 from '../../resources/img/main/main9.webp';
+import main10 from '../../resources/img/main/main10.webp';
+
+import { useState, useEffect } from 'react';
 import Spinner from '../Spinner';
 
 const Main = ({ onGetWeather, mainLoading, forecastLoading }) => {
+	const images = [main1, main2, main3, main4, main5, main6, main7, main8, main9, main10];
+	const [mainImage, setMainImage] = useState();
 	const [value, setValue] = useState('');
+
+	useEffect(() => {
+		setMainImage(images[getRandom(0, images.length - 1)])
+	}, []);
+
+	function getRandom(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	let buttonClasses;
+	if (!mainLoading || !forecastLoading) {
+		buttonClasses = "main__button main__button_active";
+	} else {
+		buttonClasses = "main__button";
+	}
 
 	return (
 		<div className="main">
-			<img src={main} alt="main" />
+			<img src={mainImage} alt="main" />
 			<div className="main__finder">
 				<input
+					disabled={mainLoading || forecastLoading ? true : false}
 					type="text"
 					placeholder='type your city'
 					value={value}
-					onChange={(e) => setValue(e.target.value)} />
-				<button disabled={mainLoading || forecastLoading ? true : false}
-					onClick={() => { onGetWeather(value) }}
-				>
-					{mainLoading ? <Spinner /> : 'Get Weather'}
+					onChange={(e) => setValue(e.target.value)}
+					onKeyDown={(e) => { e.key === 'Enter' && onGetWeather(value) }} />
+				<button
+					className={buttonClasses}
+					disabled={mainLoading || forecastLoading ? true : false}
+					onClick={() => { onGetWeather(value) }}>
+					{mainLoading ?
+						<Spinner />
+						:
+						'Get Weather'
+					}
 				</button>
 			</div>
 		</div>
