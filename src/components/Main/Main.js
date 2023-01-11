527f17cd53bd24320a7c502a9c09254ab7aff32d
Nathan <nathanbailie.dev@gmail.com>
2023-01-11 15:44:34 +0300

    made validation

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -17,7 +17,8 @@ const Main = ({ onGetWeather, mainLoading, forecastLoading, lang, setLang }) => 
 	const images = [main1, main2, main3, main4, main5, main6, main7, main8, main9, main10];
 	const [mainImage, setMainImage] = useState();
 	const [value, setValue] = useState('');
-	const [errMessage, setErrMessage] = useState(false);
+	const [error, setError] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
 
 	useEffect(() => {
 		setMainImage(images[getRandom(0, images.length - 1)]);
@@ -27,13 +28,40 @@ const Main = ({ onGetWeather, mainLoading, forecastLoading, lang, setLang }) => 
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	};
 
-	function onValidate(value) {
+	function onValidate(lang, value) {
 		if (value.length === 0) {
-			setErrMessage(true);
-		} else {
-			setErrMessage(false);
-			onGetWeather(value);
+			setError(true);
+			setErrorMessage(() => {
+				if (lang === 'ru') {
+					return 'Введите название города';
+				}
+				return 'Please, type your city';
+			});
+			return;
 		};
+		if (lang === 'ru' && /[A-Za-z]+/.test(value)) {
+			setError(true);
+			setErrorMessage(() => {
+				if (lang === 'ru') {
+					return 'Используйте кириллицу';
+				}
+				return 'Use the Latin';
+			});
+			return;
+		};
+		if (lang === 'en' && /[А-ЯЁа-яё]+/.test(value)) {
+			setError(true);
+			setErrorMessage(() => {
+				if (lang === 'ru') {
+					return 'Используйте кириллицу';
+				}
+				return 'Use the Latin';
+			});
+			return;
+		};
+
+		setError(false);
+		onGetWeather(value, lang);
 	};
 
 	let buttonClasses;
@@ -44,7 +72,7 @@ const Main = ({ onGetWeather, mainLoading, forecastLoading, lang, setLang }) => 
 	};
 
 	let errorMessageClasses;
-	if (!errMessage) {
+	if (!error) {
 		errorMessageClasses = 'main__errorMessage';
 	} else {
 		errorMessageClasses = 'main__errorMessage main__errorMessage_active';
@@ -58,25 +86,25 @@ const Main = ({ onGetWeather, mainLoading, forecastLoading, lang, setLang }) => 
 				<input
 					disabled={mainLoading || forecastLoading ? true : false}
 					type="text"
-					placeholder='type your city'
+					placeholder={lang === 'ru' ? 'введите ваш город' : 'type your city'}
 					value={value}
 					onChange={(e) => setValue(e.target.value)}
-					onKeyDown={(e) => { e.key === 'Enter' && onGetWeather(value, lang) }} />
+					onKeyDown={(e) => { e.key === 'Enter' && onValidate(lang, value) }} />
 				<button
 					className={buttonClasses}
 					disabled={mainLoading || forecastLoading ? true : false}
-					onClick={() => onValidate(value)}>
+					onClick={() => onValidate(lang, value)}>
 					{mainLoading ?
 						<Spinner />
 						:
-						'Get Weather'
+						lang === 'ru' ? 'Узнать погоду' : 'Get Weather'
 					}
 				</button>
 			</div>
-			<p className={errorMessageClasses}>Please, type your city</p>
+			<p className={errorMessageClasses}>{errorMessage}</p>
 			<select
 				className='main__select'
-				onChange={(e) => setLang(e.target.value)}>
+				onChange={(e) => { setLang(e.target.value); setError(false) }}>
 				<option>ru</option>
 				<option>en</option>
 			</select>
